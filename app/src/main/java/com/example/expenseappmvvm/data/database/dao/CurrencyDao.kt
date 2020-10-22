6a82701e133be4a7c3f49cbf0e8405fe689da8c6
Razvan Prundel <razzzvi94@yahoo.com>
2020-10-22 10:57:20 +0300

    Currency converter real time conversion + error handling

--- app/src/main/java/com/example/expenseappmvvm/data/database/dao/CurrencyDao.kt
+++ app/src/main/java/com/example/expenseappmvvm/data/database/dao/CurrencyDao.kt
@@ -3,11 +3,19 @@ package com.example.expenseappmvvm.data.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.expenseappmvvm.data.database.entities.Currency
+import io.reactivex.Maybe
 import io.reactivex.Single
 
 @Dao
 interface CurrencyDao {
-    @Insert(onConflict = OnConflictStrategy.ABORT)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertCurrency(currency: Currency): Single<Long>
+
+    @Query("SELECT currencyDate FROM currencies WHERE currencyId = 0")
+    fun getCurrencyDate(): Maybe<Long>
+
+    @Query("SELECT * FROM currencies WHERE currencyId = 0")
+    fun getCurrencyDB(): Single<Currency>
 }
--- app/src/main/java/com/example/expenseappmvvm/screens/mainScreen/HomeViewModel.kt
+++ app/src/main/java/com/example/expenseappmvvm/screens/mainScreen/HomeViewModel.kt
@@ -1,20 +1,28 @@
 package com.example.expenseappmvvm.screens.mainScreen
 
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.expenseappmvvm.R
+import com.example.expenseappmvvm.data.database.entities.Currency
+import com.example.expenseappmvvm.data.database.repositories.CurrencyRepository
 import com.example.expenseappmvvm.data.database.repositories.UserRepository
+import com.example.expenseappmvvm.data.rest.CurrencyResponse
 import com.example.expenseappmvvm.data.rx.AppRxSchedulers
-import com.example.expenseappmvvm.utils.Constants
-import com.example.expenseappmvvm.utils.Preferences
-import com.example.expenseappmvvm.utils.SingleLiveEvent
-import com.example.expenseappmvvm.utils.disposeBy
+import com.example.expenseappmvvm.network.RestServiceInterface
+import com.example.expenseappmvvm.utils.*
+import com.example.expenseappmvvm.utils.resourceUtils.ResourceUtils
 import io.reactivex.disposables.CompositeDisposable
 import timber.log.Timber
 
+
 class HomeViewModel(
+    private val resourceUtils: ResourceUtils,
     private val sharedPref: Preferences,
     private val userRepository: UserRepository,
+    private val currencyRepository: CurrencyRepository,
     private val rxSchedulers: AppRxSchedulers,
+    private val converterAPI: RestServiceInterface,
     private val compositeDisposable: CompositeDisposable
 ) : ViewModel() {
 
@@ -26,6 +34,72 @@ class HomeViewModel(
     var isExpenseTabSelected = MutableLiveData<Boolean>().apply { value = false }
 
     val userName = MutableLiveData<String>().apply { value = Constants.EMPTY_STRING }
+
+    private fun getCurrency() {
+        return converterAPI.allCurrency
+            .subscribeOn(rxSchedulers.background())
+            .observeOn(rxSchedulers.androidUI())
+            .doOnError {
+                Timber.e(it.localizedMessage)
+            }
+            .doOnNext { currencyResponse -> saveCurrencyToDB(currencyResponse) }
+            .subscribe({
+                Timber.d(resourceUtils.getStringResource(R.string.currency_retrieve_success))
+            }, {
+                Toast.makeText(
+                    resourceUtils.getContext(),
+                    resourceUtils.getStringResource(R.string.currency_retrieve_failed),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }).disposeBy(compositeDisposable)
+    }
+
+    private fun saveCurrencyToDB(currencyObj: CurrencyResponse) {
+        val currency = Currency(
+            currencyBase = currencyObj.base,
+            currencyDate = TimeUtils.currencyDateToGMT(currencyObj.date),
+            EUR = currencyObj.rates.EUR,
+            USD = currencyObj.rates.USD,
+            GBP = currencyObj.rates.GBP,
+            CHF = currencyObj.rates.CHF,
+            AUD = currencyObj.rates.AUD
+        )
+        currencyRepository.insertCurrency(currency)
+            .subscribeOn(rxSchedulers.background())
+            .observeOn(rxSchedulers.androidUI())
+            .subscribe({
+                Toast.makeText(
+                    resourceUtils.getContext(),
+                    resourceUtils.getStringResource(R.string.save_currency_success),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }, {
+                Toast.makeText(
+                    resourceUtils.getContext(),
+                    resourceUtils.getStringResource(R.string.save_currency_failed),
+                    Toast.LENGTH_SHORT
+                ).show()
+            })
+            .disposeBy(compositeDisposable)
+    }
+
+    fun getCurrencyDate() {
+        return currencyRepository.getCurrencyDate()
+            .subscribeOn(rxSchedulers.background())
+            .observeOn(rxSchedulers.androidUI())
+            .doOnSuccess {
+                if(isNetworkConnected(resourceUtils.getContext()) && TimeUtils.lastCurrencyCheck(it)){
+                    getCurrency()
+                }
+            }
+            .doOnComplete {
+                getCurrency()
+            }
+            .subscribe({
+            },{
+                Timber.e(it.localizedMessage)
+            }).disposeBy(compositeDisposable)
+    }
 
     fun onLogoutClick() {
         sharedPref.clear()
